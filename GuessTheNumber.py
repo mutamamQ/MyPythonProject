172f14c667ed662f5a39ced031bb7215ecfa4550
mutamamQ <59139862+mutamamQ@users.noreply.github.com>
2020-02-25 21:29:14 +0300

    GuessTheNumber.py
    Guess the number game I coded while procrastinating.

---
+++ GuessTheNumber.py
@@ -0,0 +1,24 @@
+import random
+def game():
+    x = random.randint(1, 100)
+    guessCount =0
+    print("I the mainframe have chosen a number.\nGuess it to win.")
+
+    while True:
+        guess = int(input())
+        #Debuging
+        if guess == 999:
+            print(x)
+        #Debuging
+        if guess == x:
+            guessCount+= 1
+            print("Congratulations you have guessed correctly. You guessed it in ",guessCount," tries")
+            break
+        else:
+            guessCount+= 1
+            print("Wrong. Try again!")
+            continue
+game()
+regame = input("do you want to play again")
+if regame == "yes":
+    game()
