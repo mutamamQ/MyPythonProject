a001799de09eb3cc03a333fcfa45100b6794fb50
mutamamQ <59139862+mutamamQ@users.noreply.github.com>
2020-02-27 08:47:52 +0300

    Create Pythonmathandinput.py

---
+++ Pythonmathandinput.py
@@ -0,0 +1,22 @@
+
+#1. exercise
+x = float(input("Enter the first performance grade."))
+y = float(input("Enter the second performance grade."))
+z = float(input("Enter the first exam grade."))
+w = float(input("Enter the second exam grade."))
+z = x+y+z+w
+z /= 4
+print("Your average grade is ", int(z))
+#2. exercise 
+
+r=float(input("what is the radius of the circle"))
+area = (r**2)*3.14 #3.14 is pi 
+print("the area is",area)
+diameter = 3.14*2*r
+print("the diameter is ", diameter) 
+#3. exercise
+x=float(input("what is a side of the triangle"))
+y=float(input("what is the other side of the triangle"))
+z = x**2 + y**2
+z=z**(1/2) 
+print("hypotenuse is"z) 
